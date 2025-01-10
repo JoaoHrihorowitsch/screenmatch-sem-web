b9c093bfcf5769577a3d422c37846475b9f3d703
João Victor Hrihorowitsch Monteiro <jvhmonteiro@gmail.com>
2025-01-10 13:34:32 -0300

    Adicionando novas alterações

--- screematch/src/main/java/br/com/alura/screematch/principal/Principal.java
+++ screematch/src/main/java/br/com/alura/screematch/principal/Principal.java
@@ -19,40 +19,27 @@ public class Principal {
     private final String API_KEY = "&apikey=9f78f39";
 
     public void exibeMenu(){
-        System.out.println("Digite a opção desejada:");
-        System.out.println("1 - Buscar série");
-        System.out.println("2 - Sair");
-        int opcao = scan.nextInt();
-        scan.nextLine();
+        System.out.println("Digite o nome da Série:");
+        var nomeSerie = scan.nextLine();
 
-        switch (opcao) {
-            case 1:
-                System.out.println("Digite o nome da Série:");
-                var nomeSerie = scan.nextLine();
+        var json = consumo.obterDados(ENDERCO + nomeSerie.replace(" ", "+") + API_KEY);
 
-                var json = consumo.obterDados(ENDERCO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
 
-                DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-                System.out.println(dados);
+        List<DadosTempoda> temporadas = new ArrayList<>();
 
-                List<DadosTempoda> temporadas = new ArrayList<>();
-
-                for(int i=1; i<= dados.totalTemporadas(); i++){
-                    json = consumo.obterDados(ENDERCO + nomeSerie.replace(" ", "+") + "&season=" + i +  API_KEY);
-                    DadosTempoda temporada = conversor.obterDados(json, DadosTempoda.class);
-                    temporadas.add(temporada);
-                }
-
-                temporadas.forEach(t -> {
-                    System.out.println("temporada " + t.temporada() + ":");
-                    t.episodioList().forEach(e -> System.out.println(e.titulo() + " - " + e.titulo()));
-                    System.out.println();
-                });
-                break;
-
-            case 2:
-                System.out.println("Encerrando progama...");
-                break;
+        for(int i=1; i<= dados.totalTemporadas(); i++){
+            json = consumo.obterDados(ENDERCO + nomeSerie.replace(" ", "+") + "&season=" + i +  API_KEY);
+            DadosTempoda temporada = conversor.obterDados(json, DadosTempoda.class);
+            temporadas.add(temporada);
         }
+
+        temporadas.forEach(t -> {
+            System.out.println("temporada " + t.temporada() + ":");
+            t.episodioList().forEach(e -> System.out.println(e.titulo() + " - " + e.titulo()));
+            System.out.println();
+        });
     }
 }
+
